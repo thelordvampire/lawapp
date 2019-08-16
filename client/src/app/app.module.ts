0df3f6d463c861b087951d2b7d3b607b5ee0abbc
tungtuan <tung.nguyentuan@ncc.asia>
2019-08-16 23:22:41 +0700

    angular structure

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './main-component/home';
+import { PracticeComponent } from './main-component/practice/practice.component';
+import { ClientsComponent } from './main-component/clients/clients.component';
+import { ResourcesComponent } from './main-component/resources/resources.component';
+import { TypoComponent } from './main-component/typo/typo.component';
+import { ContactComponent } from './main-component/contact/contact.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AlertComponent,
+        PracticeComponent,
+        ClientsComponent,
+        ResourcesComponent,
+        TypoComponent,
+        ContactComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ client/src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_helpers';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './main-component/home';
+import { PracticeComponent } from './main-component/practice/practice.component';
+import { ClientsComponent } from './main-component/clients/clients.component';
+import { ResourcesComponent } from './main-component/resources/resources.component';
+import { TypoComponent } from './main-component/typo/typo.component';
+import { ContactComponent } from './main-component/contact/contact.component';
+
+const routes: Routes = [
+    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    // { path: '', component: HomeComponent },
+    { path: '', component: AppComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'practice', component: PracticeComponent },
+    { path: 'clients', component: ClientsComponent },
+    { path: 'resources', component: ResourcesComponent },
+    { path: 'typo', component: TypoComponent },
+    { path: 'contact', component: ContactComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: 'home' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
