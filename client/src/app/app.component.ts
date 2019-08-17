abe7d3b701f1a9ada41af976280f8bd10a3e0010
tungtuan <tung.nguyentuan@ncc.asia>
2019-08-17 10:56:29 +0700

    update home page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as $ from 'jquery';
 import { AuthenticationService } from './_services';
 
-@Component({ selector: 'app', templateUrl: 'app.component.html' })
+
+@Component({ selector: 'app',
+ templateUrl: 'app.component.html',
+ styleUrls: ['./app.component.scss'] })
 export class AppComponent {
-    currentUser: any;
+    currentUser: any; 
 
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService
     ) {
+        this.router.navigate(['/home']);
        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-       this.router.navigate(['/home']);
+       $("span.menu").click(function(){
+        $(" ul.navig").slideToggle("slow" , function(){
+        });
+    });
+
+    // $(function(){
+                                     
+    // });
+    // $(window).load(function(){
+    //   $('.flexslider').flexslider({
+    //     animation: "slide",
+    //     start: function(slider){
+    //       $('body').removeClass('loading');
+    //     }
+    //   });
+    // });
+
+
+
     }
 
     logout() {
         this.authenticationService.logout();
         this.router.navigate(['/login']);
     }
-}
+} 
