494cf61fa93511854da58b2178a38aebb3a2b05a
tungtuan <tung.nguyentuan@ncc.asia>
2019-08-23 00:02:26 +0700

    add chat

--- client/src/app/main-component/chat/chat.component.ts
+++ client/src/app/main-component/chat/chat.component.ts
@@ -3,7 +3,7 @@ import * as SockJS from 'sockjs-client';
 import * as Stomp from 'stompjs';
 import { AppService } from '../../app.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import $ from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-chat',
@@ -12,26 +12,35 @@ import $ from 'jquery';
 })
 export class ChatComponent implements OnInit {
   chatForm: FormGroup;
+  isShow = false;
+  isShowHeader = true;
+  usernamePage;
+  chatPage;
+  usernameForm;
+  messageForm;
+  messageInput;
+  messageArea;
+  connectingElement;
   constructor(
     private appService: AppService,
     private fb: FormBuilder,
     ) { }
 
   ngOnInit() {
-    // this.initializeWebSocketConnection();
-    this.appService.setHeader(false);
     this.chatForm = this.fb.group({
       Username: [''],
       Message: [''],
     });
+
+    this.usernamePage = document.querySelector('#username-page');
+    this.chatPage = document.querySelector('#chat-page');
+    this.usernameForm = document.querySelector('#usernameForm');
+    this.messageForm = document.querySelector('#messageForm');
+    this.messageInput = document.querySelector('#message');
+    this.messageArea = document.querySelector('#messageArea');
+    this.connectingElement = document.querySelector('.connecting');
   }
-   usernamePage = document.querySelector('#username-page');
- chatPage = document.querySelector('#chat-page');
- usernameForm = document.querySelector('#usernameForm');
- messageForm = document.querySelector('#messageForm');
- messageInput = document.querySelector('#message');
- messageArea = document.querySelector('#messageArea');
- connectingElement = document.querySelector('.connecting');
+
 
  stompClient = null;
  username = null;
@@ -42,64 +51,46 @@ export class ChatComponent implements OnInit {
 ];
 
  connect() {
-   console.log('document.querySelector', document.querySelector('#name'));
-   // this.username = document.querySelector('#name').value.trim();
-
-   // this.usernamePage.classList.add('hidden');
-   // this.chatPage.classList.remove('hidden');
+   console.log('onConnect');
+   this.isShow = true;
    const url = 'http://localhost:8080/ws';
    const socket = new SockJS(url);
-   console.log('dfsdg', socket);
-
-
    this.stompClient = Stomp.over(socket);
-   console.log('this.chatForm', this.stompClient);
-   this.stompClient.connect({}, this.onConnected, this.onError);
-  // event.preventDefault();
+   this.stompClient.connect({}, this.onConnected.bind(this), this.onError.bind(this));
 }
 
 
  onConnected() {
+  console.log('onConnected');
   // Subscribe to the Public Topic
-  // this.stompClient.subscribe('/topic/public', onMessageReceived);
-
   // Tell your username to the server
-
-  console.log('onConected');
-
-  if (this.stompClient) {
-    this.stompClient.send('/app/chat.addUser',
-      {},
-      JSON.stringify({sender: this.username, type: 'JOIN'})
-    );
-  }
-
-  // this.connectingElement.classList.add('hidden');
+    this.stompClient.subscribe('/topic/public', this.onMessageReceived.bind(this));
+    console.log('thi111111111111ent', this.stompClient);
+    this.stompClient.send('/app/chat.addUser', {}, JSON.stringify({sender: this.username, type: 'JOIN'}));
 }
 
 
  onError(error) {
    console.log('on err');
-
   this.connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
-  // this.connectingElement.style.color = 'red';
+  this.connectingElement.style.color = 'red';
 }
 
 
  sendMessage() {
-  //  var messageContent = this.messageInput.value.trim();
-  const messageContent = 'fsdgds';
+  console.log('sendMessage');
       var chatMessage = {
-          sender: this.username,
-          content: this.messageInput,
+          sender: this.chatForm.controls.Username.value,
+          content: this.chatForm.controls.Message.value,
           type: 'CHAT'
       };
       this.stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-      // this.messageInput = '';
+      this.chatForm.controls.Message.reset();
 }
 
 
  onMessageReceived(payload) {
+   console.log('payload', payload);
   var message = JSON.parse(payload.body);
 
   var messageElement = document.createElement('li');
@@ -112,7 +103,14 @@ export class ChatComponent implements OnInit {
       message.content = message.sender + ' left!';
   } else {
       messageElement.classList.add('chat-message');
-
+      console.log('fsdjklfsdklfj', );
+      
+      if(this.chatForm.controls.Username.value == JSON.parse(payload.body).sender) {
+        messageElement.classList.add('right-text');
+      } else {
+        messageElement.classList.add('left-text');
+      }
+      
       var avatarElement = document.createElement('i');
       var avatarText = document.createTextNode(message.sender[0]);
       avatarElement.appendChild(avatarText);
@@ -131,9 +129,8 @@ export class ChatComponent implements OnInit {
   textElement.appendChild(messageText);
 
   messageElement.appendChild(textElement);
-
-  this.messageArea.appendChild(messageElement);
-  this.messageArea.scrollTop = this.messageArea.scrollHeight;
+  $('#messageArea').append(messageElement);
+  $('#messageArea').scrollTop = $('#messageArea').scrollHeight;
 }
 
 
@@ -145,26 +142,7 @@ export class ChatComponent implements OnInit {
   var index = Math.abs(hash % this.colors.length);
   return this.colors[index];
 }
-// private serverUrl = 'https://localhost:8080/ws'
-// private title = 'WebSockets chat';
-// private stompClient;
-
-// initializeWebSocketConnection(){
-//   let ws = new SockJS(this.serverUrl);
-//   this.stompClient = Stomp.over(ws);
-//   let that = this;
-//   this.stompClient.connect({}, function(frame) {
-//     that.stompClient.subscribe("/chat", (message) => {
-//       if(message.body) {
-//         $(".chat").append("<div class='message'>"+message.body+"</div>")
-//         console.log(message.body);
-//       }
-//     });
-//   });
-// }
-
-// sendMessage(message){
-//   this.stompClient.send("/app/send/message" , {}, message);
-//   $('#input').val('');
-// }
+onPressCloseHeader() {
+  this.isShowHeader = !this.isShowHeader;
+}
 }
