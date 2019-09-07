0a68bf2c1c495bb7576d9863d13ab3c0de671d08
K.Wan <chomchomcapital@gmail.com>
2019-09-08 01:39:23 +0700

    update post

---
+++ LawApplication/src/main/java/com/app/law/config/Utf8Config.java
@@ -0,0 +1,7 @@
+package com.app.law.config;
+
+/**
+ * Created by https://github.com/kwanpham
+ */
+public class Utf8Config {
+}
---
+++ LawApplication/src/main/java/com/app/law/mapper/PostMapper.java
@@ -0,0 +1,7 @@
+package com.app.law.mapper;
+
+/**
+ * Created by https://github.com/kwanpham
+ */
+public class PostMapper {
+}
